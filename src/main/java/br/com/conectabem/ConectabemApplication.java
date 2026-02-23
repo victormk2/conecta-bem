20136709d356fc1cdf578a84c7c70f6471fe630d
Victor Kruger <98160719+victormk2@users.noreply.github.com>
2026-02-23 19:06:33 -0300

    initial commit

---
+++ src/main/java/br/com/conectabem/ConectabemApplication.java
@@ -0,0 +1,13 @@
+package br.com.conectabem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConectabemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConectabemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/conectabem/ConectabemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.conectabem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConectabemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
